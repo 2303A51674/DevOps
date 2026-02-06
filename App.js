9c5e871380a1506364e5f7942725c84bd3361735
Hajira Nausheen <2303a51674@sru.edu.in>
2026-02-06 23:59:27 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import Cart from "./Cart";  // Import Cart component
-
+import React, { useState } from "react";
+import StudentProfile from "./StudentProfile";
 function App() {
+  const [showProfile, setShowProfile] = useState(true);
+  // Toggle visibility of StudentProfile
+  const toggleProfile = () => {
+    setShowProfile(!showProfile);
+  };
   return (
-    <div>
-      <Cart />  {/* Use Cart component */}
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Lifecycle Demonstration</h1>
+      <button onClick={toggleProfile}>
+        {showProfile ? "Hide" : "Show"} Student Profile
+      </button>
+      {/* Render StudentProfile only when showProfile is true */}
+      {showProfile && <StudentProfile />}
     </div>
   );
 }
-
-export default App;
+export  default  App;
